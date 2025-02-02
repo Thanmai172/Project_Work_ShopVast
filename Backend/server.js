cd270dd1824cca43f507c3c6dd0adeb3992ed095
222010301002 <222010301002thanmai@gmail.com>
2025-02-02 15:39:01 +0530

    added the server.js file

--- Backend/server.js
+++ Backend/server.js
@@ -17,8 +17,16 @@ const app = express();
 const PORT = process.env.PORT || 5000;  
 
 // Middleware  
-app.use(cors());  
-app.use(bodyParser.json());   
+// app.use(cors());  
+app.use(bodyParser.json());  
+
+app.use(
+  cors({
+    origin: "https://shopvast-project-work-app.netlify.app",
+    credentials: true,
+  })
+);
+
 
 // Database Connection  
 mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })  
